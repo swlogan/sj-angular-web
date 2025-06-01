921f3ffa09df32584b3b147252af2a8b5ed44ffd
Shawn Logan <swlogan7@gmail.com>
2025-06-01 19:56:08 -0400

    Added loading animation the webpage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { NavigationComponent } from '../navigation/navigation.component';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavigationComponent],
+  imports: [NavigationComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const loader = document.getElementById('app-loader');
+      const gif = document.getElementById('loader-gif') as HTMLImageElement;
+
+      const hide = () => {
+        if (loader) {
+          loader.classList.add('hidden');
+          setTimeout(() => (loader.style.display = 'none'), 1000);
+        }
+      };
+
+      // Hide after 4 seconds even if GIF never loads
+      setTimeout(hide, 4000);
+
+      // Also try to hide as soon as GIF is loaded
+      if (gif) {
+        if (gif.complete) {
+          hide();
+        } else {
+          gif.onload = () => hide();
+        }
+      }
+    }
+  }
+}
