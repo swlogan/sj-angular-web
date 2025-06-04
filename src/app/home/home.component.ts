8fb0a678b6ce32b3aa40ecd3c62f10b0d2d8f3ac
Shawn Logan <swlogan7@gmail.com>
2025-06-03 22:18:54 -0400

    Added div about me section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, ViewChild, ElementRef } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { NavigationComponent } from '../navigation/navigation.component';
+
 
 @Component({
   selector: 'app-home',
@@ -10,31 +11,65 @@ import { NavigationComponent } from '../navigation/navigation.component';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements AfterViewInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  @ViewChild('introCard', { static: false }) introCard!: ElementRef<HTMLDivElement>;
 
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      const loader = document.getElementById('app-loader');
-      const gif = document.getElementById('loader-gif') as HTMLImageElement;
 
-      const hide = () => {
-        if (loader) {
-          loader.classList.add('hidden');
-          setTimeout(() => (loader.style.display = 'none'), 1000);
+  typingText = [
+    'ACCESSING RESUME DATABASE...',
+    'AUTHENTICATION SUCCESSFUL',
+    'LOADING PROFILE...'
+  ];
+
+ngAfterViewInit(): void {
+  if (isPlatformBrowser(this.platformId)) {
+    const loader = document.getElementById('app-loader');
+    const gif = document.getElementById('loader-gif') as HTMLImageElement;
+    const terminalText = document.getElementById('terminal-text');
+
+    const introCard = this.introCard;
+
+const hide = () => {
+  if (loader) {
+    loader.classList.add('hidden');
+    setTimeout(() => {
+      loader.style.display = 'none';
+
+      // Animate the intro card
+      requestAnimationFrame(() => {
+        if (introCard?.nativeElement) {
+          introCard.nativeElement.classList.remove('hidden-card');
+          introCard.nativeElement.classList.add('visible-card');
         }
-      };
+      });
+    }, 1000);
+  }
+};
 
-      // Hide after 4 seconds even if GIF never loads
-      setTimeout(hide, 4000);
-
-      // Also try to hide as soon as GIF is loaded
-      if (gif) {
-        if (gif.complete) {
-          hide();
-        } else {
-          gif.onload = () => hide();
+    const runTyping = async () => {
+      if (terminalText) {
+        for (const line of this.typingText) {
+          for (let i = 0; i <= line.length; i++) {
+            terminalText.innerHTML = line.slice(0, i) + (i % 2 ? '_' : '');
+            await new Promise(res => setTimeout(res, 40));
+          }
+          terminalText.innerHTML += '<br>';
+          await new Promise(res => setTimeout(res, 600));
         }
       }
+
+      // Reveal site after typing
+      hide();
+    };
+
+    if (gif && gif.complete) {
+      runTyping();
+    } else {
+      gif.onload = () => runTyping();
     }
+
+    // Fallback
+    setTimeout(hide, 7000);
   }
+}
 }
