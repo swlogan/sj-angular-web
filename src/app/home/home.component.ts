2d21808076b5770901318e4f52bdc29ab07634d8
Shawn Logan <swlogan7@gmail.com>
2025-06-03 22:31:34 -0400

    Get profile card to fade in after loading animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,75 +1,73 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID, ViewChild, ElementRef, NgZone } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { NavigationComponent } from '../navigation/navigation.component';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [NavigationComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements AfterViewInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private ngZone: NgZone
+  ) {}
+
   @ViewChild('introCard', { static: false }) introCard!: ElementRef<HTMLDivElement>;
 
-
   typingText = [
-    'ACCESSING RESUME DATABASE...',
+    'ACCESSING RESUME DATA...',
     'AUTHENTICATION SUCCESSFUL',
-    'LOADING PROFILE...'
+    'LOADING USER PROFILE...'
   ];
 
-ngAfterViewInit(): void {
-  if (isPlatformBrowser(this.platformId)) {
-    const loader = document.getElementById('app-loader');
-    const gif = document.getElementById('loader-gif') as HTMLImageElement;
-    const terminalText = document.getElementById('terminal-text');
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const loader = document.getElementById('app-loader');
+      const gif = document.getElementById('loader-gif') as HTMLImageElement;
+      const terminalText = document.getElementById('terminal-text');
 
-    const introCard = this.introCard;
+      const hide = () => {
+        if (loader) {
+          loader.classList.add('hidden');
+          setTimeout(() => {
+            loader.style.display = 'none';
 
-const hide = () => {
-  if (loader) {
-    loader.classList.add('hidden');
-    setTimeout(() => {
-      loader.style.display = 'none';
-
-      // Animate the intro card
-      requestAnimationFrame(() => {
-        if (introCard?.nativeElement) {
-          introCard.nativeElement.classList.remove('hidden-card');
-          introCard.nativeElement.classList.add('visible-card');
+            // Force Angular to recognize the class change
+            this.ngZone.run(() => {
+              if (this.introCard?.nativeElement) {
+                this.introCard.nativeElement.classList.remove('hidden-card');
+                this.introCard.nativeElement.classList.add('visible-card');
+              }
+            });
+          }, 1000);
         }
-      });
-    }, 1000);
-  }
-};
+      };
 
-    const runTyping = async () => {
-      if (terminalText) {
-        for (const line of this.typingText) {
-          for (let i = 0; i <= line.length; i++) {
-            terminalText.innerHTML = line.slice(0, i) + (i % 2 ? '_' : '');
-            await new Promise(res => setTimeout(res, 40));
+      const runTyping = async () => {
+        if (terminalText) {
+          for (const line of this.typingText) {
+            for (let i = 0; i <= line.length; i++) {
+              terminalText.innerHTML = line.slice(0, i) + (i % 2 ? '_' : '');
+              await new Promise(res => setTimeout(res, 40));
+            }
+            terminalText.innerHTML += '<br>';
+            await new Promise(res => setTimeout(res, 600));
           }
-          terminalText.innerHTML += '<br>';
-          await new Promise(res => setTimeout(res, 600));
         }
+
+        hide();
+      };
+
+      if (gif && gif.complete) {
+        runTyping();
+      } else {
+        gif.onload = () => runTyping();
       }
 
-      // Reveal site after typing
-      hide();
-    };
-
-    if (gif && gif.complete) {
-      runTyping();
-    } else {
-      gif.onload = () => runTyping();
+      setTimeout(hide, 7000);
     }
-
-    // Fallback
-    setTimeout(hide, 7000);
   }
-}
 }
