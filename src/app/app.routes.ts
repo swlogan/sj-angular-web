7af85bfbe16f1064e9dcee778b9ef40d87e063a6
Shawn Logan <swlogan7@gmail.com>
2025-11-06 22:26:23 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
 { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'resume', component: ResumeComponent },
+  { path: 'contact', component: ContactComponent}
   // Add more routes as needed
 ];
