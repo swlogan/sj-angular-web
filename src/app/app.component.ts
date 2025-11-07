b9ca5d684ac00d9430a112d67f9c25103f1603ed
Shawn Logan <swlogan7@gmail.com>
2025-11-06 22:26:21 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   template: `
     <router-outlet></router-outlet>
